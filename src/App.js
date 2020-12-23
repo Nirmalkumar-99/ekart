ead359d4f4eefb20673ab414ca5a987a20911700
Nirmalkumar-99 <nirmalkumar.anbalagan@nutechnologyinc.com>
2020-12-23 18:47:53 +0530

    babel commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  var detail=
+  {
+    name:"welcome"
+  };
+
+    const [Toggle, state] = useState(true);
+
+    const toggleHandler = () => {
+      const temp = Toggle;
+      state(!temp);
+    }
+    let displayContent = detail.name+" Hello.. this is react!!";
+    if (Toggle) {
+      displayContent = "bye..";
+    }
+ 
+    return (
+
+    <div>
+      <button onClick={toggleHandler}>clickme</button>
+      <div className="content">
+        {displayContent}
+      </div>
     </div>
   );
-}
+    }
+
+
 
 export default App;
